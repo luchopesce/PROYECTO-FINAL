30b24375f7f9f4bd0eeb1b85d1ae1c35432b92fd
luchopesce <luchopesce96@gmail.com>
2022-09-11 18:19:19 -0300

    entrega segunda parte

--- assets/js/storejs.js
+++ assets/js/storejs.js
@@ -1,35 +1,46 @@
 //-------------FUNCIONES GENERALES------------------------
 document.addEventListener("DOMContentLoaded", function(){
     get_datos();
-    insertar_datos();
+    pr_tabla_admin();
+    pr_productos();
+    scrollFunction();
 })
 
-//-------------BOTONES------------------------
+//-------------ACCIONES------------------------  
+let tabla_productos = document.getElementById("pr_tabla");
 let btn_pr_crear = document.getElementById("btn_pr_crear");
+let btn_login = document.getElementById("btn_login")
+let btn_search_box = document.getElementById("pr_search_box");
+let read_file = document.getElementById('pr_fileimg');
+let tier_item = document.getElementById("pr_item_tier");
+let scroll_bar = document.getElementById("top");
+
 if(btn_pr_crear){
     btn_pr_crear.addEventListener("click", set_datos);
 }
 
-let btn_login = document.getElementById("btn_login")
 if(btn_login){
-    btn_login.addEventListener("click", check_login)
+    btn_login.addEventListener("click", (e)=>{
+        e.preventDefault();
+        check_login()
+    })
 }
 
-const read_file = document.getElementById('pr_fileimg');
+if (btn_search_box){
+    btn_search_box.addEventListener("keyup", ()=>{ search(tabla_productos) });
+}
+
 if(read_file){
     read_file.addEventListener('change', (e)=>{
-        const file = e.target.files[0];
-        const reader = new FileReader();
+        let file = e.target.files[0];
+        let reader = new FileReader();
         reader.onloadend = () => {
-            var base64String = reader.result.replace('data:', '').replace(/^.+,/, '');
-            img.push(base64String);
+            let base64String = reader.result.replace('data:', '').replace(/^.+,/, '');
+            lista_pr_img.push(base64String);
         };
         reader.readAsDataURL(file);
     })
 }
-
-let img = [];
-console.log(img);
 
 //-------------ARRAYS Y CLASES------------------------
 let usuarios = [{
@@ -37,8 +48,10 @@ let usuarios = [{
     pass: "admin"
 }]
 let lista_productos = [];
+let lista_pr_img = [];
+
 class Producto{
-    constructor(id, nombre, precio, desc, stock, categoria, file){
+    constructor(id, nombre, precio, desc, stock, categoria, file, oldprice){
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
@@ -46,6 +59,7 @@ class Producto{
         this.stock = stock;
         this.categoria = categoria;
         this.file = file;
+        this.oldprice = oldprice;
     }
     show_producto(){
         console.log("-------------Producto creado con exito--------------");
@@ -55,12 +69,13 @@ class Producto{
         console.log("El id del producto generado es: ", this.id);
         console.log("El desc del producto es: ", this.desc);
         console.log("La categoria del producto es: ", this.categoria);
+        console.log("El precio con el descuento es:", this.oldprice);
     }
 }
 
 //guarda en la variable length la cantidad de digitos de un entero
 function validate_num(num){
-    var length = ~~(Math.log(num) / Math.LN10 + 1); 
+    let length = ~~(Math.log(num) / Math.LN10 + 1); 
     return length;
 }
 
@@ -126,8 +141,19 @@ function set_datos () {
     }
 
     let pr_id = generar_random_id(lista_productos);
+    if(!pr_id){
+        return;
+    }
 
-    let datos = new Producto(pr_id, pr_nombre, pr_precio, pr_descuento, pr_stock, pr_categoria, img[0])
+    if(!lista_pr_img[0]){
+        return;
+    }
+
+    pr_oldprice = pr_precio + (pr_precio * (pr_descuento / 100));
+    pr_oldprice = new Intl.NumberFormat('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(pr_oldprice)
+    pr_precio = new Intl.NumberFormat('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(pr_precio)
+
+    let datos = new Producto(pr_id, pr_nombre, pr_precio, pr_descuento, pr_stock, pr_categoria, lista_pr_img[0], pr_oldprice)
 
     lista_productos.push(datos);
     datos.show_producto();
@@ -142,7 +168,6 @@ function get_datos(){
         recibir_producto = JSON.parse(recibir_producto);
         for(let productos of recibir_producto){
             lista_productos.push(productos);
-            console.log(lista_productos);
         }
     }
     else{
@@ -150,34 +175,131 @@ function get_datos(){
     }
 }
 
-let tabla_productos = document.getElementById("pr_tabla");
-function insertar_datos(){
+function pr_tabla_admin(){
     if(tabla_productos){
         for (let producto of lista_productos){
+            let img_id = producto.nombre + producto.id;
             let file_img = `data:image/png;base64,${producto.file}`;
             let th_producto = document.createElement("tr");
-            th_producto.innerHTML = `<td>${producto.id}</td>
-            <td>${producto.nombre}</td>
-            <td>${producto.precio}</td>
-            <td>${producto.desc}</td>
-            <td>${producto.categoria}</td>
-            <td>${producto.stock}</td>
-            <td><a>
-            <img id="viewImage" class="img_fluid" onClick="openImage();" src="${file_img}" width="50" height="50">
-            </a>
-            </td>`
+            th_producto.classList.add("pr_items");
+            th_producto.innerHTML = `<td class="align-middle text-primary pr_id">${producto.id}</td>
+                                    <td class="align-middle text-primary pr_name">${producto.nombre}</td>
+                                    <td class="align-middle text-primary"><span class="text-primary">$</span> ${producto.precio}</td>
+                                    <td class="align-middle text-primary">${producto.desc} <span class="text-primary">%</span> </td>
+                                    <td class="align-middle text-primary">${producto.stock}</td>
+                                    <td class="align-middle text-primary pr_categoria">${producto.categoria}</td>
+                                    <td class="d-flex justify-content-center">
+                                    <div class="table_img">
+                                    <img id="${producto.id}" class="btn" src="${file_img}" alt="" style="max-height: 75px;">
+                                    </div>
+                                    <div id="${producto.nombre}" class="modal-img">
+                                    <span class="close-img">&times;</span>
+                                    <div class="d-flex justify-content-center">
+                                    <img class="modal-content-id" id="${img_id}"></div>
+                                    </div>
+                                    </div>
+                                    </td>`
             tabla_productos.append(th_producto);
+            let modal = document.getElementById(producto.nombre);
+            let pr_img = document.getElementById(producto.id);
+            let modalImg = document.getElementById(img_id);
+            pr_img.onclick = function(){
+                modal.style.display = "block";
+                modalImg.src = this.src;
+            }
+            let btns_modal = modal.getElementsByClassName("close-img");
+            for (let i = 0; i < btns_modal.length; i++) {
+                btns_modal[i].addEventListener("click", function(){
+                    modal.style.display = "none";
+                });
+            }              
         }
     }
 }
 
-function openImage() {
-    var largeImage = document.getElementById('viewImage');
-    largeImage.style.display = 'block';
-    largeImage.style.width = 200 + "px";
-    largeImage.style.height = 200 + "px";
+function search(el){
+    let search_box = document.getElementById("pr_search_box").value;
+    let pr_producto = document.querySelectorAll(".pr_items");
+    let pr_name = el.getElementsByClassName("pr_name");
+    let pr_id = el.getElementsByClassName("pr_id");
+    let pr_categoria = el.getElementsByClassName("pr_categoria");
 
-    var w = window.open();
-    w.document.write(largeImage.outerHTML);
-    w.document.close(); 
+    for (let i = 0; i < pr_id.length || i < pr_name.length || i < pr_categoria.length; i++){
+        let acc_search_name = pr_producto[i].getElementsByClassName("pr_name")[0];
+        let acc_search_id = pr_producto[i].getElementsByClassName("pr_id")[0];
+        let acc_search_categoria = pr_producto[i].getElementsByClassName("pr_categoria")[0];
+        if (acc_search_name || acc_search_id || acc_search_categoria){
+            let text_search = acc_search_name.textContent || acc_search_name.innerHTML;
+            let text_search_id = acc_search_id.textContent || acc_search_id.innerHTML;
+            let text_search_categoria = acc_search_categoria.textContent || acc_search_categoria.innerHTML;
+            if (text_search.toUpperCase().indexOf(search_box.toUpperCase()) > -1){
+                pr_producto[i].style.display = "";
+            }
+            else if (text_search_id.indexOf(search_box) > -1){
+                pr_producto[i].style.display = "";
+            }
+            else if (text_search_categoria.toUpperCase().indexOf(search_box.toUpperCase()) > -1){
+                pr_producto[i].style.display = "";
+            }
+            else{
+                pr_producto[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function pr_productos(){
+    let path = window.location.pathname;
+    let page = path.split("/").pop();
+    let pr_item_filter = lista_productos.filter(el => el.categoria.toLowerCase() === page.toLowerCase())
+    if(lista_productos.some(el => el.categoria.toLowerCase() === page.toLowerCase())){
+        for(let pr_item of pr_item_filter){
+            let file_img = `data:image/png;base64,${pr_item.file}`;
+            let div_producto = document.createElement("div");
+            div_producto.classList.add("col");
+            div_producto.innerHTML = `<div class="card gn_main-card text-center">
+                                    <h5 class="card-title mt-2">${pr_item.nombre}</h5>
+                                    <a href="">
+                                    <img src="${file_img}" alt="...">
+                                    </a>
+                                    <div class="card-body">
+                                    <div class="card-oldprice">
+                                    <span class="h6">$</span>
+                                    <span class="h6">${pr_item.oldprice}</span>
+                                    </div>
+                                    <div class="card-newprice">
+                                    <span class="h4">$</span>
+                                    <span class="h4">${pr_item.precio}</span>
+                                    <span class="card-newprice-off">${pr_item.desc}% OFF</span>
+                                    </div>    
+                                    </div>
+                                    <div class="card-footer gn_main-propag-item">
+                                    <div class="d-flex justify-content-center">
+                                    <a href="" data-bs-toggle="modal" data-bs-target="#modal_medios-pagos">
+                                    <p class="mb-1 mt-1">Hasta 12 cuotas sin interés</p>
+                                    </a>
+                                    </div>
+                                    </div>
+                                    </div>`
+            tier_item.append(div_producto);
+        }
+    }
+}
+
+function scrollFunction() {
+    if (scroll_bar){
+        let prevScrollpos = window.pageYOffset;
+        window.onscroll = function() {
+            if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+                let navbar = document.getElementById('top');
+                let currentScrollPos = window.pageYOffset;
+                    if (prevScrollpos > currentScrollPos) {
+                        navbar.classList.remove("scrolled");
+                    } else {
+                        navbar.classList.add("scrolled");
+                    }
+                    prevScrollpos = currentScrollPos;
+            }
+        }
+    }
 }
